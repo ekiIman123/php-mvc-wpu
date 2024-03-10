d3fcbd2d8b9e436e7efe27ea19e8610aae8820c3
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-03-10 23:01:43 +0700

    first

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+  <div class="jumbotron mt-5">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Halo nama saya <?= $data['nama']; ?></p>
+    <hr class="my-4">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, exercitationem.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+  </div>
+
+</div>
