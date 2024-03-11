9d1935131f2f0be1e87601242e5880945dcf4769
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-03-11 12:56:33 +0700

    second

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,14 +1,41 @@
-<div class="container mt-5">
+<div class="container mt-3">
 
   <div class="row">
-    <div class="col-6">
+    <div class="col-lg-6">
+      <?php Flasher::flash(); ?>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-lg-6">
+      <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
+        Tambah Data Mahasiswa
+      </button>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-lg-6">
+      <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Cari Mahasiswa..." name="keyword" id="keyword" autocomplete="off">
+          <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-6">
       <h3>Daftar Mahasiswa</h3>
 
       <ul class="list-group">
         <?php foreach( $data['mhs'] as $mhs ) : ?>
-          <li class="list-group-item d-flex justify-content-between align-items-start">
+          <li class="list-group-item">
             <?= $mhs['nama']; ?>
-            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">detail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge text-bg-danger float-end float-right ms-1 text-decoration-none" onClick="return confirm('yakin?');">hapus</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge text-bg-success float-end ms-1 text-decoration-none tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id']; ?>">ubah</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary float-end ms-1 text-decoration-none">detail</a>
           </li>
         <?php endforeach; ?>
       </ul>
@@ -17,4 +44,54 @@
     </div>
   </div>
   
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="formModalLabel">Tambah Data Mahasiswa</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+          <input type="hidden" name="id" id="id">
+          <div class="mb-3">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+          </div>
+
+          <div class="mb-3">
+            <label for="nrp" class="form-label">NRP</label>
+            <input type="number" class="form-control" id="nrp" name="nrp">
+          </div>
+
+          <div class="mb-3">
+            <label for="email" class="form-label">email</label>
+            <input type="email" class="form-control" id="email" name="email">
+          </div>
+          
+          <div class="form-group">
+            <label for="jurusan" class="form-label">Jurusan</label>
+            <select class="form-select" id="jurusan" name="jurusan">
+              <option selected>Open this select menu</option>
+              <option value="Teknik Informatika">Teknik Informatika</option>
+              <option value="Teknik Elektro">Teknik Elektro</option>
+              <option value="Teknik Kimia">Teknik Kimia</option>
+              <option value="Teknik Geologi">Teknik Geologi</option>
+              <option value="Teknik Metalurgi">Teknik Metalurgi</option>
+              <option value="Teknik Planalogi">Teknik Planalogi</option>
+            </select>
+          </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
